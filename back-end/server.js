f7842ad6f0122ca86d24e787bf0d61a02c834b4a
Smit90 <45687440+Smit90@users.noreply.github.com>
2022-09-26 14:01:46 +0530

    added chat API

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,8 @@ const connectDB = require("./config/db");
 var colors = require("colors");
 const path = require("path");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 // dotenv.config();
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
@@ -14,10 +16,15 @@ const app = express();
 app.use(express.json()); // to accept json data
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to chat app");
 });
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
