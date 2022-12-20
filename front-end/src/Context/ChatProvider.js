d73249472fb8214fee3be221e97694e08d1b53cd
Smit90 <45687440+Smit90@users.noreply.github.com>
2022-12-20 09:56:56 +0530

    fix socket endpoint

--- front-end/src/Context/ChatProvider.js
+++ front-end/src/Context/ChatProvider.js
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 import { useHistory, useNavigate } from "react-router-dom";
 
 import io from "socket.io-client";
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = process.env.REACT_APP_SOCKET_ENDPOINT;
 var socket;
 
 const ChatContext = createContext();
